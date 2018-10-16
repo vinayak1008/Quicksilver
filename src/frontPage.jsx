ad65cc83278bccb2876d409eb4d7cdaa68ee8d6c
Vinayak <vinayakbhagat.10@gmail.com>
2018-10-16 20:31:28 +0530

    install react

---
+++ src/frontPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Nav from "./frontPage/nav";
+
+const FrontPage = () => (
+  <div className="main-page">
+    <Nav />
+  </div>
+);
+export default FrontPage;
---
+++ src/frontPage/nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./nav.scss";
+// import  from "..//";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div className="">hello</div>
+      </div>
+    );
+  }
+}
+export default Nav;
